15cfea4ea79d471cd5a984c7902ccb42c84eb3c6
W.M.P.I.Dharmagunawardana <it21132346@my.sliit.lk>
2024-10-21 23:54:48 +0530

    commit

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -14,8 +14,9 @@ export const Header = (props) => {
         <div className="overlay">
           <div className="container">
             <div className="row">
-              <div className="col-md-8 col-md-offset-2 intro-text">
+              <div className="col-md-8 col-md-offset-2 intro-text"> 
                 <h1>
+
                   {homeData.title}
                   <span></span>
                 </h1>
@@ -24,7 +25,7 @@ export const Header = (props) => {
                   href="#features"
                   className="btn btn-custom btn-lg page-scroll"
                 >
-                  Learn More
+                  Learn More!
                 </a>
               </div>
             </div>
